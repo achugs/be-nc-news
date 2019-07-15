a7626278af4044ee4df501a240786c2c4443479d
Achugs <anna_9400@hotmail.com>
2019-07-15 19:10:32 +0100

    utils completed

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -6,12 +6,19 @@ exports.formatDates = list => {
 };
 
 exports.makeRefObj = list => {
-  if (!list.length) return [];
   const obj = {};
   list.forEach(id => (obj[id.title] = id.article_id))
   return obj;
 };
 
 exports.formatComments = (comments, articleRef) => {
-  return [];
+  return comments.map(comment => {
+    const { created_by, created_at, belongs_to, ...rest } = comment;
+    const obj = { ...rest };
+    obj.author = created_by;
+    obj.article_id = articleRef[belongs_to];
+    obj.created_at = new Date(created_at);
+    console.log(new Date(created_at))
+    return obj;
+  });
 };
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -59,8 +59,8 @@ describe('formatDates', () => {
 });
 
 describe('makeRefObj', () => {
-  it('returns an empty array when passed an empty array', () => {
-    expect(makeRefObj([])).to.eql([]);
+  it('returns an empty object when passed an empty array', () => {
+    expect(makeRefObj([])).to.eql({});
   });
   it('returns an object with they key of article_id changed to the titles value when pased an array', () => {
     const input = [{ article_id: 1, title: 'Student SUES Mitch!' }];
@@ -79,5 +79,80 @@ describe('makeRefObj', () => {
 describe('formatComments', () => {
   it('returns an empty array when passed an empty array', () => {
     expect(formatComments([])).to.eql([]);
+  });
+  it('takes an array of comment objects and a reference object, and returns a new array of formatted comments.', () => {
+    const input = [{
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      created_at: 1511354163389
+    }]
+    const articleRef = { "They're not exactly dogs, are they?": 1 };
+    const output = [{
+      article_id: 1,
+      author: 'butter_bridge',
+      created_at: new Date(1511354163389)
+    }]
+    expect(formatComments(input, articleRef)).to.eql(output)
+  });
+  it('takes an array of different comment objects and a reference object, and returns a new array of formatted comments.', () => {
+    const input = [{
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      created_at: 1511354163389
+    }, {
+      belongs_to: "Living in the shadow of a great man",
+      created_by: 'butter_bridge',
+      created_at: 1479818163389
+    }
+    ]
+    const articleRef = { "They're not exactly dogs, are they?": 1, 'Living in the shadow of a great man': 2 };
+    const output = [{
+      article_id: 1,
+      author: 'butter_bridge',
+      created_at: new Date(1511354163389)
+    },
+    {
+      article_id: 2,
+      author: 'butter_bridge',
+      created_at: new Date(1479818163389)
+    }
+    ]
+    expect(formatComments(input, articleRef)).to.eql(output)
+  });
+  it('takes an array of different comment objects and a reference object, and returns a new array of formatted comments-- for full comment keys', () => {
+    const input = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      belongs_to: "They're not exactly dogs, are they?",
+      created_by: 'butter_bridge',
+      votes: 16,
+      created_at: 1511354163389,
+    },
+    {
+      body:
+        'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+      belongs_to: 'Living in the shadow of a great man',
+      created_by: 'butter_bridge',
+      votes: 14,
+      created_at: 1479818163389,
+    }]
+    const articleRef = { "They're not exactly dogs, are they?": 1, 'Living in the shadow of a great man': 2 };
+    const expected = [{
+      body:
+        "Oh, I've got compassion running out of my nose, pal! I'm the Sultan of Sentiment!",
+      article_id: 1,
+      author: 'butter_bridge',
+      votes: 16,
+      created_at: new Date(1511354163389),
+    },
+    {
+      body:
+        'The beautiful thing about treasure is that it exists. Got to find out what kind of sheets these are; not cotton, not rayon, silky.',
+      article_id: 2,
+      author: 'butter_bridge',
+      votes: 14,
+      created_at: new Date(1479818163389),
+    }]
+    expect(formatComments(input, articleRef)).to.eql(expected);
   });
 });
