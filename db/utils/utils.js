6e55c0bdc3677ca032c9d5b30b91fb0b01f7945d
Achugs <anna_9400@hotmail.com>
2019-07-15 16:31:09 +0100

    formatted dates and refObject utils created

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,7 +1,17 @@
 exports.formatDates = list => {
-  return [];
+  return list.map(date => {
+    date.created_at = new Date(date.created_at)
+    return date
+  })
 };
 
-exports.makeRefObj = list => { };
+exports.makeRefObj = list => {
+  if (!list.length) return [];
+  const obj = {};
+  list.forEach(id => (obj[id.title] = id.article_id))
+  return obj;
+};
 
-exports.formatComments = (comments, articleRef) => { };
+exports.formatComments = (comments, articleRef) => {
+  return [];
+};
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -10,10 +10,74 @@ describe('formatDates', () => {
     expect(formatDates([])).to.eql([]);
   });
   it('returns an array with the sql timestamp changed to js when passed an array', () => {
-    const input = 
+    const input = [{
+      created_at: 1542284514171
+    }]
+    const expected = [{
+      created_at: new Date(1542284514171)
+    }]
+    expect(formatDates(input)).to.eql(expected);
+  });
+  it('returns an array of objects with timestamp changed to js', () => {
+    const input = [{
+      created_at: 1542284514171
+    }, { created_at: 1416140514171 }]
+    const expected = [{
+      created_at: new Date(1542284514171)
+    }, { created_at: new Date(1416140514171) }]
+    expect(formatDates(input)).to.eql(expected);
+  });
+  it('returns an array of objects with timestamp changed to js with other elements added to the objects', () => {
+    const input = [{
+      title: 'Living in the shadow of a great man',
+      created_at: 1542284514171
+
+    },
+    {
+      title: 'Sony Vaio; or, The Laptop',
+      created_at: 1416140514171,
+    },
+    {
+      title: 'Eight pug gifs that remind me of mitch',
+      created_at: 1289996514171
+    }]
+    const expected = [{
+      title: 'Living in the shadow of a great man',
+      created_at: new Date(1542284514171)
+
+    },
+    {
+      title: 'Sony Vaio; or, The Laptop',
+      created_at: new Date(1416140514171),
+    },
+    {
+      title: 'Eight pug gifs that remind me of mitch',
+      created_at: new Date(1289996514171)
+    }]
+    expect(formatDates(input)).to.eql(expected);
   });
 });
 
-describe('makeRefObj', () => { });
+describe('makeRefObj', () => {
+  it('returns an empty array when passed an empty array', () => {
+    expect(makeRefObj([])).to.eql([]);
+  });
+  it('returns an object with they key of article_id changed to the titles value when pased an array', () => {
+    const input = [{ article_id: 1, title: 'Student SUES Mitch!' }];
+    const output = { 'Student SUES Mitch!': 1 };
+    expect(makeRefObj(input)).to.eql(output);
+  });
+  it('returns objects with they key of article_id changed to the titles value returns an array', () => {
+    const input = [{ article_id: 1, title: 'Student SUES Mitch!' }, {
+      article_id: 2, title: 'A'
+    }];
+    const output = { 'Student SUES Mitch!': 1, 'A': 2 };
+    expect(makeRefObj(input)).to.eql(output);
+  });
+});
 
-describe('formatComments', () => { });
+describe('formatComments', () => {
+  it('returns an empty array when passed an empty array', () => {
+    expect(formatComments([])).to.eql([]);
+  });
+});
