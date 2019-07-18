3f43ba58433ca731f924562a7e8ab05f5366a801
Achugs <anna_9400@hotmail.com>
2019-07-18 11:07:20 +0100

    changed comment_count: article.articleid to comments.articleid

--- models/article-model.js
+++ models/article-model.js
@@ -36,7 +36,7 @@ exports.getArticles = ({ sort_by, order = 'desc' }) => {
   return connection
     .select('articles.author', 'title', 'articles.article_id', 'topic', 'articles.created_at', 'articles.votes')
     .from('articles')
-    .count({ comment_count: 'articles.article_id' })
+    .count({ comment_count: 'comments.article_id' })
     .leftJoin('comments', 'articles.article_id', 'comments.article_id')
     .groupBy('articles.article_id')
 
